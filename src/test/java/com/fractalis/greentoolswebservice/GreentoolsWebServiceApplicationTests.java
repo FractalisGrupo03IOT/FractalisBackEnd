b32831e08ec3e1f83e36c5cbdc51d1881de1f6b0
Lino Abraham Quenta <89022192+linoabraham@users.noreply.github.com>
2024-11-02 18:54:04 -0500

    Update GreentoolsWebServiceApplicationTests.java
    test

--- src/test/java/com/fractalis/greentoolswebservice/GreentoolsWebServiceApplicationTests.java
+++ src/test/java/com/fractalis/greentoolswebservice/GreentoolsWebServiceApplicationTests.java
@@ -1,13 +1,1 @@
-package com.fractalis.greentoolswebservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GreentoolsWebServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
