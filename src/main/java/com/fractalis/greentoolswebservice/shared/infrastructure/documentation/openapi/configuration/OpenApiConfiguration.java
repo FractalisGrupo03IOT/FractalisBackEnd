6395bf7b771ed9708499d067be33e79e0b6953c1
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2024-11-02 02:12:33 -0500

    open api config

---
+++ src/main/java/com/fractalis/greentoolswebservice/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,30 @@
+package com.fractalis.greentoolswebservice.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI FractilesEdgePlatformOpenApi() {
+        var openApi = new OpenAPI();
+
+        openApi
+                .info(new Info()
+                        .title("Fractalies API Platform")
+                        .description(" Fractalies API Platform application REST API documentation.")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("www")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Fractalies")
+                        .url(""));
+
+        return openApi;
+    }
+}
