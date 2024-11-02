5adbaf46594600b928d858981703604185c1ad06
GioRC0 <142363773+GioRC0@users.noreply.github.com>
2024-11-02 01:17:09 -0500

    cors localhost 4200

--- src/main/java/com/fractalis/greentoolswebservice/shared/infrastructure/cors/configuration/WebConfig.java
+++ src/main/java/com/fractalis/greentoolswebservice/shared/infrastructure/cors/configuration/WebConfig.java
@@ -13,8 +13,9 @@ public class WebConfig implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry registry)
     {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
