db6c508f1c0038d1c8c40c06b4a68d7633c2b389
Lino Abraham Quenta <89022192+linoabraham@users.noreply.github.com>
2024-11-02 18:56:31 -0500

    Update PlantQueryService.java

--- src/main/java/com/fractalis/greentoolswebservice/cropStatus/domain/services/PlantQueryService.java
+++ src/main/java/com/fractalis/greentoolswebservice/cropStatus/domain/services/PlantQueryService.java
@@ -1,1 +1,14 @@
+package com.fractalis.greentoolswebservice.cropStatus.domain.services;
+
+import com.fractalis.greentoolswebservice.cropStatus.domain.model.aggregates.Plant;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlantQueryService {
+    Optional<Plant> getPlantById(Long plantId);
+    List<Plant> getPlantsByStationId(Long stationId);
+    List<Plant> getAllPlants();
+
+}
 
