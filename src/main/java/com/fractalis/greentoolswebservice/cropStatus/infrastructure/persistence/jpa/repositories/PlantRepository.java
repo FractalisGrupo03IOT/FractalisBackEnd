6eb11b932343905d9b27051d1a8c5fb2d3dcd012
JPatrickSR <102174297+JPatrickSR@users.noreply.github.com>
2024-11-03 03:08:13 -0500

    Update PlantRepository.java
    v2.1

--- src/main/java/com/fractalis/greentoolswebservice/cropStatus/infrastructure/persistence/jpa/repositories/PlantRepository.java
+++ src/main/java/com/fractalis/greentoolswebservice/cropStatus/infrastructure/persistence/jpa/repositories/PlantRepository.java
@@ -2,7 +2,7 @@ package com.fractalis.greentoolswebservice.cropStatus.infrastructure.persistence
 
 import com.fractalis.greentoolswebservice.cropStatus.domain.model.aggregates.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository; 
  
 import java.util.List;
 
