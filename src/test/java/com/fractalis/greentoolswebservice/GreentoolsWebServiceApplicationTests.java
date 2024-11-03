37765044eb2d46140a1b578631424cf6ae2362a0
JPatrickSR <102174297+JPatrickSR@users.noreply.github.com>
2024-11-03 03:07:36 -0500

    Update GreentoolsWebServiceApplicationTests.java
    v2.2

--- src/test/java/com/fractalis/greentoolswebservice/GreentoolsWebServiceApplicationTests.java
+++ src/test/java/com/fractalis/greentoolswebservice/GreentoolsWebServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.fractalis.greentoolswebservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest 
 class GreentoolsWebServiceApplicationTests {
  
     @Test
